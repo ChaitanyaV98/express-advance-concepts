0f02b260b9eb84f06221e7f3ce69a467084c4f5d
ChaitanyaV98 <chaitanya@betalectic.com>
2025-07-03 16:57:35 +0530

    added item routes so as to understand how our middlewares are helpful for the scenarios like error handling, ratelimiting, showing custom logs, api verioning etc..

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import configureCors from "./config/corsConfig.js";
 import { addTimeStamp, reqLogger } from "./middleware/customMiddleware.js";
 import { globalErrorHandler } from "./middleware/errorHandler.js";
 import { urlVersioning } from "./middleware/apiVersioning.js";
+import { itemRoutes } from "./routes/item-routes.js";
+import { createBasicRateLimiter } from "./middleware/rateLimiting.js";
 
 const port = process.env.PORT || 3000;
 
@@ -12,12 +14,15 @@ const app = express();
 app.use(reqLogger); // this will help showing our req details
 app.use(addTimeStamp); // this will add timestamp to the req obj
 
+app.use(createBasicRateLimiter(20, 15 * 60 * 1000)); // 2 request per 15 minutes
+
 //added configure cors middleware
 app.use(configureCors());
 //express json middleware
 app.use(express.json());
 
 app.use(urlVersioning("v1"));
+app.use("/api/v1", itemRoutes);
 
 app.use(globalErrorHandler);
 
